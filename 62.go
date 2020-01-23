1b13ef6609cf5977dcdbaf768367b6610a4785a0
zr <zhengrui.l@gmail.com>
2020-01-23 14:17:47 +0800

    problem 62: left shift looply not right shift

---
+++ 62.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	var (
+		newHead *ListNode
+		length  int
+	)
+	if head == nil {
+		return head
+	}
+
+	//find the tail
+	p := head
+	for p.Next != nil {
+		p = p.Next
+		length++
+	}
+	//make the ring circle
+	p.Next = head
+
+	cnt := k % (length + 1)
+	p = head
+	i := 0
+	for {
+		if i == cnt {
+			newHead = p
+		} else if i > k && p.Next == newHead {
+			//break the ring circle
+			p.Next = nil
+			break
+		}
+
+		i++
+		p = p.Next
+	}
+	return newHead
+}
+
+func main() {
+	node0 := &ListNode{0, nil}
+	node1 := &ListNode{1, nil}
+	node2 := &ListNode{2, nil}
+	node0.Next = node1
+	node1.Next = node2
+	p := rotateRight(node0, 2)
+
+	for p != nil {
+		fmt.Print(p.Val, " ")
+		p = p.Next
+	}
+	fmt.Println()
+}
