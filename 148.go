260499cd3d1cfff6c0e1cc3519fc98f512ca92f6
zr <zhengrui.l@gmail.com>
2020-01-27 18:21:06 +0800

    change to non-recursively bottom-to-top merge algorithm

--- 148.go
+++ 148.go
@@ -14,37 +14,83 @@ func sortList(head *ListNode) *ListNode {
 		return head
 	}
 
-    //move to the middle node
-	slow, fast := head, head.Next
-	for fast != nil && fast.Next != nil {
-		slow = slow.Next
-		fast = fast.Next
-        fast = fast.Next
+	dummy := &ListNode{0, head}
+	intv, length := 1, 0
+	l, r := head, head
+	for r != nil {
+		r = r.Next
+		length++
 	}
 
-    //get left and right ordered list recursively
-	right := slow.Next
-	slow.Next = nil
-	l, r := sortList(head), sortList(right)
+	for intv < length {
+		p := dummy
+		for {
+			l, r = p.Next, p.Next
+			for i := 0; i < intv; i++ {
+				if r == nil {
+					break
+				}
+				r = r.Next
+			}
 
-    //merge left and right ordered list together
-	dummy := &ListNode{0, nil}
-	head = dummy
-	for l != nil && r != nil {
-		if l.Val <= r.Val {
-			head.Next = l
-			head, l = head.Next, l.Next
-		} else {
-			head.Next = r
-			head, r = head.Next, r.Next
+			//merge left and right ordered list together
+			lnum, rnum := 0, 0
+			for lnum < intv && rnum < intv {
+				if l == nil || r == nil {
+					break
+				}
+				if l.Val <= r.Val {
+					p.Next = l
+					p, l = p.Next, l.Next
+					lnum++
+				} else {
+					p.Next = r
+					p, r = p.Next, r.Next
+					rnum++
+				}
+			}
+            //p always point to the last ordered node
+            //link the remain of r
+			if rnum < intv && r != nil {
+				p.Next = r
+                //iterate to the end of r
+				for ; rnum < intv; rnum++ {
+                    //r is short than intv
+                    if p.Next == nil {
+                        break
+                    }
+					p = p.Next
+				}
+			}
+            //link the remain of l
+			if lnum < intv {
+				q := p.Next
+				p.Next = l
+                //iterate to the end of l
+				for ; lnum < intv; lnum++ {
+                    //l is short than intv
+                    if p.Next == nil {
+                        break
+                    }
+					p = p.Next
+				}
+				if r == nil {
+					p.Next = nil
+				} else {
+					p.Next = q
+				}
+			}
+
+			if r == nil {
+				break
+			}
 		}
+
+		head = dummy.Next
+		intv *= 2
+		//fmt.Println(getValFromListNode(head))
 	}
-	if l != nil {
-		head.Next = l
-	}
-	if r != nil {
-		head.Next = r
-	}
+
 	return dummy.Next
 }
 
@@ -77,8 +123,9 @@ func getValFromListNode(head *ListNode) []int {
 }
 
 func main() {
+	//head := createListNode([]int{3, 2, 4})
 	//head := createListNode([]int{3, 4, 1})
-	head := createListNode([]int{-1, 5, 3, 4, 0})
+	head := createListNode([]int{-1, 5, 2, 3, 0})
 	//head := createListNode([]int{4, 2, 1, 3})
 	//head := createListNode([]int{3, 1})
 	//head := createListNode([]int{3})
