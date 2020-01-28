e0579ffaed3b55e75b3a484d0c6ed788d3017977
zr <zhengrui.l@gmail.com>
2020-01-29 00:26:36 +0800

    works ok for big intergers sum

--- 445.go
+++ 445.go
@@ -10,33 +10,49 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	num1, num2 := 0, 0
+	num1, num2 := make([]int, 0), make([]int, 0)
 	p := l1
 	for p != nil {
-		num1 *= 10
-		num1 += p.Val
+		num1 = append(num1, p.Val)
 		p = p.Next
 	}
 
 	p = l2
 	for p != nil {
-		num2 *= 10
-		num2 += p.Val
+		num2 = append(num2, p.Val)
 		p = p.Next
 	}
 
-	num := num1 + num2
 	p = &ListNode{0, nil}
-	for num > 0 {
-		q := &ListNode{num % 10, nil}
+	carry := 0
+	for i, j := len(num1)-1, len(num2)-1; carry > 0 || i >= 0 || j >= 0; i, j = i-1, j-1 {
+		q := &ListNode{0, nil}
+		a, b := 0, 0
+		switch {
+		case i >= 0 && j >= 0:
+			a, b = num1[i], num2[j]
+		case i < 0 && j >= 0:
+			b = num2[j]
+		case i >= 0 && j < 0:
+			a = num1[i]
+		}
+
+		q.Val = a + b + carry
+		if q.Val >= 10 {
+			carry = 1
+			q.Val -= 10
+		} else {
+            carry = 0
+        }
+
 		q.Next = p.Next
 		p.Next = q
-		num /= 10
 	}
-    //sum is zero
-    if p.Next == nil {
-        return p
-    }
+
+	//sum is zero
+	if p.Next == nil {
+		return p
+	}
 	return p.Next
 }
 
@@ -69,8 +85,8 @@ func getValFromListNode(head *ListNode) []int {
 }
 
 func main() {
-	t1 := []int{0}
-	t2 := []int{0}
+	t1 := []int{1}
+	t2 := []int{9}
 	//t1 := []int{7, 2, 4, 3}
 	//t2 := []int{5, 6, 4}
 	l1 := createListNode(t1)
