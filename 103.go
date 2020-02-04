0b08eeea4faeba797d6f957cb7c9cc1de4607ab3
zr <zhengrui.l@gmail.com>
2020-02-04 10:43:20 +0800

    fix MakeTreeNode function bug

--- 103.go
+++ 103.go
@@ -16,15 +16,15 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 	stack := make([]*TreeNode, 0)
 	p := root
 
-    if p == nil {
-        return [][]int{}
-    }
+	if p == nil {
+		return [][]int{}
+	}
 
 	prevStack = append(prevStack, p)
 	for {
 		val := make([]int, 0)
 		if len(prevStack) > 0 {
-			for i := len(prevStack)-1; i >= 0; i-- {
+			for i := len(prevStack) - 1; i >= 0; i-- {
 				p = prevStack[i]
 				val = append(val, p.Val)
 				if p.Left != nil {
@@ -40,7 +40,7 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 				break
 			}
 		} else if len(stack) > 0 {
-			for i := len(stack)-1; i >= 0; i-- {
+			for i := len(stack) - 1; i >= 0; i-- {
 				p = stack[i]
 				val = append(val, p.Val)
 				if p.Right != nil {
@@ -63,38 +63,48 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 
 func MakeTreeNode(vals []int) *TreeNode {
 	var root, node0, node1, node2 *TreeNode
-	stack := make([]*TreeNode, 0)
+	queue := make([]*TreeNode, 0)
+	i := 0
 
-	for i := 0; i < len(vals); {
-		if len(stack) == 0 {
-			node0 = &TreeNode{vals[i], nil, nil}
-			if i == 0 {
-				root = node0
+	for {
+		if i == 0 {
+			if i >= len(vals) {
+				return root
 			}
-			stack = append(stack, node0)
+			node0 = &TreeNode{vals[i], nil, nil}
+			root = node0
+			queue = append(queue, node0)
 			i++
-		} else {
-			node1, node2 = nil, nil
-			node0 = stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
+			continue
+		}
+
+		node0Num, childNum := len(queue), 0
+		for j := 0; j < node0Num; j++ {
+			node0 = queue[j]
+
+			if i >= len(vals) {
+				return root
+			}
 			if vals[i] != -1 {
 				node1 = &TreeNode{vals[i], nil, nil}
 				node0.Left = node1
+				queue = append(queue, node1)
+				childNum++
 			}
-			if vals[i+1] != -1 {
-				node2 = &TreeNode{vals[i+1], nil, nil}
+			i++
+
+			if i >= len(vals) {
+				return root
+			}
+			if vals[i] != -1 {
+				node2 = &TreeNode{vals[i], nil, nil}
 				node0.Right = node2
+				queue = append(queue, node2)
+				childNum++
 			}
-
-			if node2 != nil {
-				stack = append(stack, node2)
-			}
-			if node1 != nil {
-				stack = append(stack, node1)
-			}
-
-			i += 2
+			i++
 		}
+		queue = queue[node0Num:]
 	}
 
 	return root
@@ -102,6 +112,7 @@ func MakeTreeNode(vals []int) *TreeNode {
 
 func main() {
 	root := MakeTreeNode([]int{3, 9, 20, -1, -1, 15, 7})
+	//root := MakeTreeNode([]int{3, 9, 20, 11, 12, 15, 7})
 	ans := zigzagLevelOrder(root)
 	for i := 0; i < len(ans); i++ {
 		fmt.Println(ans[i])
