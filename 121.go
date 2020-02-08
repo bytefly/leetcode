3ec12e77f72a6bfc8d080b354faf68a571a645b9
zr <zhengrui.l@gmail.com>
2020-02-08 17:15:44 +0800

    CPU optimized for problem 121

--- 121.go
+++ 121.go
@@ -5,15 +5,22 @@ import (
 )
 
 func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	minPrice := prices[0]
 	max := 0
 	for i := 0; i < len(prices); i++ {
-		for j := i + 1; j < len(prices); j++ {
-			if max < prices[j]-prices[i] {
-				max = prices[j] - prices[i]
+		if minPrice >= prices[i] {
+			minPrice = prices[i]
+		} else {
+			t := prices[i] - minPrice
+			if t > max {
+				max = t
 			}
 		}
 	}
-
 	return max
 }
 
