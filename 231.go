cb25f5a6eb810188567864eeea4a5aa4de15fc60
zr <zhengrui.l@gmail.com>
2020-02-11 23:07:27 +0800

    problem 231: power of two

---
+++ 231.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isPowerOfTwo(n int) bool {
+	if n == 0 {
+		return false
+	}
+	if n&(n-1) == 0 {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	fmt.Println(isPowerOfTwo(0))
+	fmt.Println(isPowerOfTwo(1))
+	fmt.Println(isPowerOfTwo(16))
+	fmt.Println(isPowerOfTwo(218))
+}
