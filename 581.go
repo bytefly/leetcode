3fab8ecdf8858857cbab008d78fbd3b3de322115
zr <zhengrui.l@gmail.com>
2020-02-12 14:55:30 +0800

    optimized for problem 581

--- 581.go
+++ 581.go
@@ -2,28 +2,40 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func findUnsortedSubarray(nums []int) int {
-	newNums := make([]int, len(nums))
-	copy(newNums, nums)
-	sort.Ints(newNums)
+	var left, right int
+	min := int(^uint32(0) >> 1)
+	max := int(^min)
 
-	left, right := 0, len(nums)-1
-	for left < right {
-		if newNums[left] == nums[left] {
-			left++
+	// find the minimum decreae value from start
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > nums[i] && nums[i] < min {
+			min = nums[i]
 		}
-		if newNums[right] == nums[right] {
-			right--
+	}
+	// find the maximum increase value from end
+	for i := len(nums) - 2; i >= 0; i-- {
+		if nums[i+1] < nums[i] && nums[i] > max {
+			max = nums[i]
 		}
-
-		if newNums[left] != nums[left] && newNums[right] != nums[right] {
+	}
+	// find the right place of minimum value
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > min {
+			left = i
 			break
 		}
 	}
-
+	// find the right place of maximum value
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] < max {
+			right = i
+			break
+		}
+	}
+	// no moving for ordered array
 	if left == right {
 		return 0
 	}
@@ -41,4 +53,5 @@ func main() {
 	fmt.Println(findUnsortedSubarray([]int{6, 2, 8, 10, 15}))
 	fmt.Println(findUnsortedSubarray([]int{2, 6, 8, 10, 15, 14}))
 	fmt.Println(findUnsortedSubarray([]int{6, 2, 8, 10, 15, 14}))
+	fmt.Println(findUnsortedSubarray([]int{1, 3, 8, 2, 6, 3, 5, 9}))
 }
