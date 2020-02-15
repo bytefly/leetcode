e69a27f8a934dee2cd8e30e756b47fdcbe54270b
zr <zhengrui.l@gmail.com>
2020-02-15 17:48:51 +0800

    optimized for problem 169 using Boyer-Moore algothrim

--- 169.go
+++ 169.go
@@ -5,20 +5,26 @@ import (
 )
 
 func majorityElement(nums []int) int {
-	m := make(map[int]int)
-	for _, num := range nums {
-		m[num]++
-	}
-	for k, v := range m {
-		if v > len(nums)/2 {
-			return k
+	cnt := 1
+	ans := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if cnt == 0 {
+			ans = nums[i]
+			cnt = 1
+			continue
+		}
+		if ans != nums[i] {
+			cnt--
+		} else {
+			cnt++
 		}
 	}
-	return 0
+	return ans
 }
 
 func main() {
 	fmt.Println(majorityElement([]int{3}))
 	fmt.Println(majorityElement([]int{3, 2, 3}))
+	fmt.Println(majorityElement([]int{10, 9, 9, 9, 10}))
 	fmt.Println(majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
 }
