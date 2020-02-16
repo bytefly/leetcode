77bcb6173a21d4d83bb3b27183d77673150dbada
zr <zhengrui.l@gmail.com>
2020-02-16 18:33:22 +0800

    problem 219: contains duplicate II

---
+++ 219.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	ints := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		for j := i; j < i+k+1 && j < len(nums); j++ {
+			if _, ok := ints[nums[j]]; ok {
+				return true
+			} else {
+				ints[nums[j]]++
+			}
+		}
+		for k, _ := range ints {
+			delete(ints, k)
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(containsNearbyDuplicate([]int{1, 2, 3, 1}, 3))
+	fmt.Println(containsNearbyDuplicate([]int{1, 0, 1, 1}, 1))
+	fmt.Println(containsNearbyDuplicate([]int{1, 2, 3, 1, 2, 3}, 2))
+	fmt.Println(containsNearbyDuplicate([]int{1}, 0))
+	fmt.Println(containsNearbyDuplicate([]int{99, 99}, 2))
+}
