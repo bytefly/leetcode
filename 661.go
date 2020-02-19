4d114a0981143159e639e1c788641e8e28400c90
zr <zhengrui.l@gmail.com>
2020-02-19 12:09:33 +0800

    remove useless code for problem 661

--- 661.go
+++ 661.go
@@ -4,34 +4,6 @@ import (
 	"fmt"
 )
 
-/*
-func imageSmoother(M [][]int) [][]int {
-	var ans [][]int
-	for i := 0; i < len(M); i++ {
-        ans = append(ans, make([]int, len(M[0]))
-    }
-	for i := 0; i < len(M); i++ {
-		for j := 0; j < len(M[0]); j++ {
-			row[i-1][j-1] += M[i][j]
-			ans[i-1][j] = M[i][j]
-			ans[i-1][j+1] += M[i][j]
-
-			row[i][j-1] += M[i][j]
-			ans[i][j] = M[i][j]
-			ans[i][j+1] += M[i][j]
-
-			row[i+1][j-1] += M[i][j]
-			row[i+1][j] += M[i][j]
-			row[i+1][j+1] += M[i][j]
-		}
-	}
-	for i := 0; i < len(M); i++ {
-		for j := 0; j < len(M[0]); j++ {
-            if i ==0 || i == len(M)-1
-        }
-	return ans
-}
-*/
 func imageSmoother(M [][]int) [][]int {
 	var ans [][]int
 	for i := 0; i < len(M); i++ {
