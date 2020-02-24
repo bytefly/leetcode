29cc9844e83f607f40ea263d1fef18f0b76262fe
zr <zhengrui.l@gmail.com>
2020-02-24 14:48:37 +0800

    optimized for problem 1287 using binary search

--- 1287.go
+++ 1287.go
@@ -3,22 +3,43 @@ package main
 import "fmt"
 
 func findSpecialInteger(arr []int) int {
-	ans := arr[0]
-	cnt, maxCnt := 1, 1
-
-	for i := 1; i < len(arr); i++ {
-		if arr[i] == arr[i-1] {
-			cnt++
-			if cnt > maxCnt {
-				ans = arr[i]
-				maxCnt = cnt
-			}
-		} else {
-			cnt = 1
+	size := len(arr) / 4
+	for i := 1; i <= 2; i++ {
+		id := size * i
+		left := searchLeft(arr, arr[id])
+		right := searchRight(arr, arr[id])
+		if right-left >= size {
+			return arr[id]
 		}
 	}
 
-	return ans
+	return arr[size*3]
+}
+
+func searchLeft(arr []int, v int) int {
+	left, right := 0, len(arr)
+	for left < right {
+		mid := (left + right) / 2
+		if arr[mid] >= v {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func searchRight(arr []int, v int) int {
+	left, right := 0, len(arr)
+	for left < right {
+		mid := (left + right) / 2
+		if arr[mid] > v {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left - 1
 }
 
 func main() {
