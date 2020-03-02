1e1b03f6c656a3af60a9ab7e025cba299e7c0a07
zr <zhengrui.l@gmail.com>
2020-03-02 15:31:19 +0800

    optimized for problem 1064 with binary search

--- 1064.go
+++ 1064.go
@@ -3,16 +3,31 @@ package main
 import "fmt"
 
 func fixedPoint(A []int) int {
-	for i := 0; i < len(A); i++ {
-		if A[i] == i {
-			return i
+	left, right := 0, len(A)-1
+	for left < right {
+		mid := left + (right-left)/2
+		if A[mid] >= mid {
+			right = mid
+		} else {
+			left = mid + 1
 		}
 	}
+	if A[left] == left {
+		return left
+	}
+
 	return -1
 }
 
 func main() {
+	fmt.Println(fixedPoint([]int{-10}))
+	fmt.Println(fixedPoint([]int{0, 2, 3, 4}))
+	fmt.Println(fixedPoint([]int{-10, -5, 0, 3}))
+	fmt.Println(fixedPoint([]int{-10, -5, 0, 2, 4}))
 	fmt.Println(fixedPoint([]int{-10, -5, 0, 3, 7}))
 	fmt.Println(fixedPoint([]int{0, 2, 5, 8, 17}))
 	fmt.Println(fixedPoint([]int{-10, -5, 3, 4, 7, 9}))
+	fmt.Println(fixedPoint([]int{-10, -5, 3, 4, 7, 9, 10}))
+	fmt.Println(fixedPoint([]int{-10, -5, -2, 0, 4, 5, 6, 7, 8, 9, 10}))
+	fmt.Println(fixedPoint([]int{-10, -5, 2, 3, 7}))
 }
