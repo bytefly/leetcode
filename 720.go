5090470cbf89e24b2721aa858a160f6a952f53da
zr <zhengrui.l@gmail.com>
2020-03-10 11:25:38 +0800

    optimized for problem 720 with trie tree

--- 720.go
+++ 720.go
@@ -2,27 +2,53 @@ package main
 
 import "fmt"
 
-func longestWord(words []string) string {
-	var ans string
-	var i, maxLen int
+type TrieNode struct {
+	IsKey    bool
+	Children [26]*TrieNode
+}
 
-	m := make(map[string]int, len(words))
-	for i := 0; i < len(words); i++ {
-		m[words[i]] = i
-	}
-
+func CreateTrieNode(words []string) *TrieNode {
+	var root TrieNode
 	for _, word := range words {
-		for i = 1; i <= len(word); i++ {
-			if _, ok := m[word[:i]]; !ok {
-				break
+		p := &root
+		for i := 0; i < len(word); i++ {
+			if p.Children[word[i]-'a'] == nil {
+				p.Children[word[i]-'a'] = &TrieNode{}
+			}
+			if i == len(word)-1 {
+				p.Children[word[i]-'a'].IsKey = true
+			} else {
+				p = p.Children[word[i]-'a']
 			}
 		}
+	}
+	return &root
+}
 
-		if i > len(word) && len(word) >= maxLen {
-			if len(word) > maxLen || word < ans {
-				maxLen = len(word)
-				ans = word
-			}
+func (this TrieNode) Search(word string) bool {
+	p := &this
+	for i := 0; i < len(word); i++ {
+		if p.Children[word[i]-'a'] == nil {
+			return false
+		}
+		if p.Children[word[i]-'a'].IsKey == false {
+			return false
+		}
+		p = p.Children[word[i]-'a']
+	}
+	return true
+}
+
+func longestWord(words []string) string {
+	var ans string
+
+	root := CreateTrieNode(words)
+	for _, word := range words {
+		if !root.Search(word) || len(word) < len(ans) {
+			continue
+		}
+		if len(word) > len(ans) || word < ans {
+			ans = word
 		}
 	}
 
@@ -32,4 +58,5 @@ func longestWord(words []string) string {
 func main() {
 	fmt.Println(longestWord([]string{"w", "wo", "wor", "worl", "world"}))
 	fmt.Println(longestWord([]string{"a", "banana", "app", "appl", "ap", "apply", "apple"}))
+	fmt.Println(longestWord([]string{"m", "mo", "moc", "moch", "mocha", "l", "la", "lat", "latt", "latte", "c", "ca", "cat"}))
 }
