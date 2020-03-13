59049e71ae20272cf4f51c415dea4a19dc9cea3d
zr <zhengrui.l@gmail.com>
2020-03-13 16:34:30 +0800

    problem 422: valid word square

---
+++ 422.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func validWordSquare(words []string) bool {
+	for i := 0; i < len(words); i++ { // row index
+		for j := 0; j < len(words[i]); j++ { //column index
+			//a string is too long, rows are short
+			if j == len(words) {
+				return false
+			}
+			//rows are too long, string is short
+			if i >= len(words[j]) {
+				return false
+			}
+			if words[j][i] != words[i][j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(validWordSquare([]string{"abcd", "bnrt", "crmy", "dtye"}))
+	fmt.Println(validWordSquare([]string{"abcd", "bnrt", "crm", "dt"}))
+	fmt.Println(validWordSquare([]string{"ball", "area", "read", "lady"}))
+	fmt.Println(validWordSquare([]string{"ball", "asee", "let", "lep"}))
+	fmt.Println(validWordSquare([]string{"ball", "asee", "letp", "le"}))
+	fmt.Println(validWordSquare([]string{"abc", "b"}))
+	fmt.Println(validWordSquare([]string{"ab", "b", "c"}))
+	fmt.Println(validWordSquare([]string{"abcd", "bef", "cfga", "d"}))
+}
