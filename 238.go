f3889cad504a33af8e672c740fa1654dcd1987b7
zr <zhengrui.l@gmail.com>
2020-03-30 10:56:38 +0800

    optimized for problem 238 without extra memory

--- 238.go
+++ 238.go
@@ -4,20 +4,18 @@ import "fmt"
 
 func productExceptSelf(nums []int) []int {
 	ans := make([]int, len(nums))
-	l := make([]int, len(nums))
-	r := make([]int, len(nums))
 
-	l[0], r[len(r)-1] = 1, 1
+	ans[0] = 1
 	for i := 1; i < len(nums); i++ {
-		l[i] = nums[i-1] * l[i-1]
-	}
-	for i := len(r) - 2; i >= 0; i-- {
-		r[i] = nums[i+1] * r[i+1]
+		ans[i] = nums[i-1] * ans[i-1]
 	}
 
-	for i := 0; i < len(nums); i++ {
-		ans[i] = l[i] * r[i]
+	r := 1
+	for i := len(nums) - 2; i >= 0; i-- {
+		r *= nums[i+1]
+		ans[i] *= r
 	}
+
 	return ans
 }
 
